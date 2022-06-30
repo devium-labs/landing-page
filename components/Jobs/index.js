5f886d55bc2a6e949a955c331d944040bb8a9cc6
Agência Ade! 25 <web@agenciaade.com.br>
2022-06-30 09:57:21 -0300

    removing jobs

--- components/Jobs/index.js
+++ components/Jobs/index.js
@@ -4,37 +4,37 @@ export default function Jobs() {
   const [numJobs, setNumJobs] = useState(2);
 
   function handleMoreJobs(e) {
-    if (numJobs + 2 >= jobs.length) e.target.classList.add("tw-hidden")
+    if (numJobs + 2 >= jobs.length) e.target.classList.add("tw-hidden");
     setNumJobs(numJobs + 2);
   }
 
   const jobs = [
-    {
-      titulo: "TopWay",
-      descricao:
-        "Neste projeto para a empresa Topway, que vende peças para caminhões, buscamos deixar o site o mais limpo e intuitivo possível, com a temáticas das cores da logo.",
-      imagem: "/topway.svg",
-      link: "topway.com",
-    },
-    {
-      titulo: "Chacará Berté",
-      descricao:
-        "O projeto para a chácara, pensado com um estilo mais casual, para a divulgação de eventos e festas.",
-      imagem: "/chacara.svg",
-      link: "chacara.com",
-    },
+    // {
+    //   titulo: "TopWay",
+    //   descricao:
+    //     "Neste projeto para a empresa Topway, que vende peças para caminhões, buscamos deixar o site o mais limpo e intuitivo possível, com a temáticas das cores da logo.",
+    //   imagem: "/topway.svg",
+    //   link: "topway.com",
+    // },
+    // {
+    //   titulo: "Chacará Berté",
+    //   descricao:
+    //     "O projeto para a chácara, pensado com um estilo mais casual, para a divulgação de eventos e festas.",
+    //   imagem: "/chacara.svg",
+    //   link: "chacara.com",
+    // },
     {
       titulo: "Ibrás",
       descricao:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nibh justo, malesuada sit amet ultrices quis, faucibus in metus. Praesent tempor eros tellus, in iaculis dolor iaculis quis. ",
-      imagem: "/ibras.svg",
+      imagem: "/ibras.png",
       link: "ibras.com",
     },
     {
       titulo: "App Smartraining",
       descricao:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nibh justo, malesuada sit amet ultrices quis, faucibus in metus. Praesent tempor eros tellus, in iaculis dolor iaculis quis. ",
-      imagem: "/app_smart.svg",
+        "Projeto desenvolvido como freelance, onde foi feito a adição de novas funcionalidades ao aplicativo existente, abrangendo os conhecimentos no desenvolvimento mobile.",
+      imagem: "/app.png",
       link: "smartraining.com",
     },
   ];
@@ -59,18 +59,20 @@ export default function Jobs() {
               </div>
               <div className="md:tw-w-2/4 tw-text-white tw-px-[20px] tw-py-[50px] tw-flex tw-items-center tw-justify-center">
                 <div className="tw-flex tw-flex-col tw-items-end tw-max-w-[400px] tw-w-full">
-                <h1 className={`tw-w-full tw-text-4xl tw-text-start`}>{job.titulo}</h1>
-                <p className="tw-text-start tw-mt-5">{job.descricao}</p>
-                <div className="tw-w-full tw-h-full tw-flex tw-pt-[20px]">
-                <a
-                  href={job.link}
-                  target="_blank"
-                  rel="noreferrer"
-                  className="tw-text-[#7bcaec] tw-border-b-[3px] tw-border-[#7bcaec]"
-                >
-                  Saiba mais
-                </a>
-                </div>
+                  <h1 className={`tw-w-full tw-text-4xl tw-text-start`}>
+                    {job.titulo}
+                  </h1>
+                  <p className="tw-text-start tw-mt-5">{job.descricao}</p>
+                  <div className="tw-w-full tw-h-full tw-flex tw-pt-[20px]">
+                    <a
+                      href={job.link}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="tw-text-[#7bcaec] tw-border-b-[3px] tw-border-[#7bcaec]"
+                    >
+                      Saiba mais
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
